e04f561be7a2cf184166f9c5d0ecc10abf9d1b51
alexyangmq <121325968+alexyangmq@users.noreply.github.com>
2022-12-26 15:42:18 -0800

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,39 @@
+import json
+import logging
+import boto3
+import requests
+def lambda_handler(event, context):   
+    print(event)
+    print(context)
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.debug(json.dumps(event))
+    codepipeline = boto3.client('codepipeline')
+    s3 = boto3.client('s3')
+    job_id = event['CodePipeline.job']['id']
+    try:
+        user_parameters = event['CodePipeline.job']['data']['actionConfiguration']['configuration']['UserParameters']
+        logger.info(f'User parameters: {user_parameters}')
+        url = event['CodePipeline.job']['data']['actionConfiguration']['configuration']['UserParameters']
+        response = requests.get(url)
+        print(response)
+        logger.info(f'codepipeline.put_job_failure_result: {job_id}')
+        response = codepipeline.put_job_failure_result(
+            jobId=job_id,
+            failureDetails={
+                'type': 'JobFailed',
+                'message': f'mock'
+            }
+        )
+        logger.debug(response)
+    except Exception as error:
+        logger.exception(error)
+        response = codepipeline.put_job_failure_result(
+            jobId=job_id,
+            failureDetails={
+                'type': 'JobFailed',
+                'message': f'{error.__class__.__name__}: {str(error)}'
+            }
+        )
+        logger.debug(response)
+
